2ad0dc71d7099a10ac0ae6757ea92815ff5acb89
xyh20001018 <1031466692@qq.com>
2023-03-27 11:32:30 +0800

    已完成侧边栏的注释

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -1,11 +1,24 @@
 <template>
-  <div class="sidebar-logo-container" :class="{'collapse':collapse}">
+  <div class="sidebar-logo-container" :class="{ 'collapse': collapse }">
+    <!-- 要进行动画的话 就要用transition标签包裹住 -->
+    <!-- 
+      transition：Vue 的内置动画标签
+      transition 标签只能包含 1 个元素；如果里面写了多个元素，则只生效第一个
+      transition 包裹的标签需要设置 v-show / v-if 属性控制元素的显示
+      
+      transform是 转换，指的是改变所在元素的外观，它有很多种手段(转换函数)来改变外观，例如 位移、缩放、旋转 等，而其中的位移的函数名就叫translate，所以说，translate是transform的一部分。
+      transition是 过渡，指的是某个CSS属性值如何平滑的进行改变，就是平常说的 动效。而transform是没有动画效果，你改变了它的值，元素的样子就唰的改变了。
+     -->
     <transition name="sidebarLogoFade">
+      <!-- 如果collapse为true 则运行这里 -->
       <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/">
+        <!-- 如果折叠了 并且有logo这个变量 则显示logo 否则只显示title -->
         <img v-if="logo" :src="logo" class="sidebar-logo">
         <h1 v-else class="sidebar-title">{{ title }} </h1>
       </router-link>
+      <!-- 如果collapse为false 则运行这里 -->
       <router-link v-else key="expand" class="sidebar-logo-link" to="/">
+        <!-- 如果没有折叠的话 有logo显示logo 没有就不显示 但是title一定显示 -->
         <img v-if="logo" :src="logo" class="sidebar-logo">
         <h1 class="sidebar-title">{{ title }} </h1>
       </router-link>
--- src/router/index.js
+++ src/router/index.js
@@ -36,6 +36,34 @@ import bak from "./bak.js";
     breadcrumb: false            if set false, the item will hidden in breadcrumb(default is true)
     activeMenu: '/example/list'  if set path, the sidebar will highlight the path you set
   }
+  
+
+  // 当设置 true 的时候该路由不会在侧边栏出现 如401，login等页面，或者如一些编辑页面/edit/1
+  hidden: true // (默认 false)
+
+  //当设置 noRedirect 的时候该路由在面包屑导航中不可被点击
+  redirect: 'noRedirect'
+
+  // 当你一个路由下面的 children 声明的路由大于1个时，自动会变成嵌套的模式--如组件页面
+  // 只有一个时，会将那个子路由当做根路由显示在侧边栏--如引导页面
+  // 若你想不管路由下面的 children 声明的个数都显示你的根路由
+  // 你可以设置 alwaysShow: true，这样它就会忽略之前定义的规则，一直显示根路由
+  alwaysShow: true
+
+  name: 'router-name' // 设定路由的名字，一定要填写不然使用<keep-alive>时会出现各种问题
+  meta: {
+    roles: ['admin', 'editor'] // 设置该路由进入的权限，支持多个权限叠加
+    title: 'title' // 设置该路由在侧边栏和面包屑中展示的名字
+    icon: 'svg-name' // 设置该路由的图标，支持 svg-class，也支持 el-icon-x element-ui 的 icon
+    noCache: true // 如果设置为true，则不会被 <keep-alive> 缓存(默认 false)
+    breadcrumb: false //  如果设置为false，则不会在breadcrumb面包屑中显示(默认 true)
+    affix: true // 如果设置为true，它则会固定在tags-view中(默认 false)
+
+    // 当路由设置了该属性，则会高亮相对应的侧边栏。
+    // 这在某些场景非常有用，比如：一个文章的列表页路由为：/article/list
+    // 点击文章进入文章详情页，这时候路由为/article/1，但你想在侧边栏高亮文章列表的路由，就可以进行如下设置
+    activeMenu: '/article/list'
+  }
  */
 
 /**
@@ -43,7 +71,7 @@ import bak from "./bak.js";
  * a base page that does not have permission requirements
  * all roles can be accessed
  */
-// canstantRoutes是静态路由
+// canstantRoutes是静态路由 代表那些不需要动态判断权限的路由，如登录页、404、等通用页面。
 // 本项目，把路由分成了两大模块：静态路由、动态路由
 // 静态路由：所有的用户可以访问，不需要全新啊
 // 动态路由: 需要权限，如果有权限，就可以访问，如果没权限，就不能访问
@@ -89,7 +117,7 @@ export const constantRoutes = [
   {
     path: "/",
     component: Layout,
-    redirect: "/dashboard",
+    redirect: "/dashboard", //重定向地址，在面包屑中点击会重定向去的地址
     children: [
       {
         path: "dashboard",
@@ -134,7 +162,11 @@ export const constantRoutes = [
         path: "index",
         component: () => import("@/views/profile/index"),
         name: "Profile",
-        meta: { title: "Profile", icon: "user", noCache: true },
+        meta: {
+          title: "Profile",
+          icon: "user",
+          noCache: true, // 不会被 <keep-alive> 缓存
+        },
       },
     ],
   },
@@ -144,11 +176,34 @@ export const constantRoutes = [
  * asyncRoutes
  * the routes that need to be dynamically loaded based on user roles
  */
-//  asyncRoutes是动态路由
+//  asyncRoutes是动态路由 代表那些需求动态判断权限并通过 addRoutes 动态添加的页面。
 //  页面级（路由级）权限：
 //    不同用户，登录到系统，看到的侧边栏是不一样的，也就是有不同的页面
 //    同一个页面，有的用户可以访问，有的用户可以访问
 export const asyncRoutes = [
+  {
+    path: "/waiter",
+    component: Layout,
+    name: "waiters",
+    redirect: "waiter/inedx",
+    alwaysShow: true,
+    meta: {
+      title: "员工管理",
+      roles: ["admin"],
+      icon: "people",
+    },
+    children: [
+      {
+        path: "index",
+        component: () => import("@/views/waiters/index"),
+        name: "waiter",
+        meta: {
+          title: "员工列表",
+          icon: "icon",
+        },
+      },
+    ],
+  },
   {
     path: "/good",
     component: Layout,
@@ -222,6 +277,7 @@ export const asyncRoutes = [
   tableRouter,
   ...bak,
   // 404 page must be placed at the end !!!
+  // 如果这里有一个需要非常注意的地方就是 404 页面一定要最后加载，如果放在 constantRoutes 一同声明了 404 ，后面的所有页面都会被拦截到404x
   { path: "*", redirect: "/404", hidden: true },
 ];
 
